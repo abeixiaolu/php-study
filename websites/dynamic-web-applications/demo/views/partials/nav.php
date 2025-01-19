dd4cb74d152c5181403d7ea707a98186db0f5e84
xiaoluabei <xiaoluabei@gmail.com>
2025-01-19 18:30:40 +0800

    Make a PHP Router

--- websites/dynamic-web-applications/demo/views/partials/nav.php
+++ websites/dynamic-web-applications/demo/views/partials/nav.php
@@ -9,8 +9,8 @@
           <div class="ml-10 flex items-baseline space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="/" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/') ? 'text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>" aria-current="page">Home</a>
-            <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about.php') ? 'text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
-            <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact.php') ? 'text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
+            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about.php') ? 'text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
+            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact.php') ? 'text-white bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
           </div>
         </div>
       </div>
