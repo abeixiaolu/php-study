24dc119fbc7fa0d692efda8840d5697d7348d1b6
xiaoluabei <xiaoluabei@gmail.com>
2025-01-20 10:27:51 +0800

    PDO First Step

--- websites/dynamic-web-applications/demo/index.php
+++ websites/dynamic-web-applications/demo/index.php
@@ -2,4 +2,13 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+// Connect to the database dsn: data source name
+$dsn = "mysql:host=localhost;dbname=myapp;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("SELECT * from posts;");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
