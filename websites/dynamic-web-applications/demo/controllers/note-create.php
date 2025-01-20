fabf6fb12b5826621505eee814dbfbac34bd8623
xiaoluabei <xiaoluabei@gmail.com>
2025-01-20 22:44:17 +0800

    Intro to form validation

--- websites/dynamic-web-applications/demo/controllers/note-create.php
+++ websites/dynamic-web-applications/demo/controllers/note-create.php
@@ -3,10 +3,26 @@ $heading = "Create Note";
 $config = require "config.php";
 $db = new Database($config['database']);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $db->query("insert into notes(body, user_id) values(:body, :user_id);", [
-    'body' => $_POST['body'],
-    'user_id' => 1,
-  ]);
+
+  $body = $_POST['body'];
+  $errors = [];
+
+  if (strlen($body) === 0) {
+    $errors['body'] = 'A body is required';
+  }
+
+  if (strlen($body) > 1000) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required';
+  }
+
+  if (empty($errors)) {
+    $db->query("insert into notes(body, user_id) values(:body, :user_id);", [
+      'body' => $body,
+      'user_id' => 1,
+    ]);
+    $body = "";
+    $errors = [];
+  }
 }
 
 require "views/note-create.view.php";
--- websites/dynamic-web-applications/demo/views/note-create.view.php
+++ websites/dynamic-web-applications/demo/views/note-create.view.php
@@ -14,8 +14,13 @@
             <div class="col-span-full">
               <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
               <div class="mt-2">
-                <textarea name="body" id="body" rows="3" placeholder="Here is an idea for a note..." class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                <textarea name="body" id="body" rows="3"
+                  placeholder="Here is an idea for a note..."
+                  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= $body ?? '' ?></textarea>
               </div>
+              <?php if (isset($errors['body'])): ?>
+                <p class="mt-2 text-sm/6 text-red-500"><?= $errors['body'] ?></p>
+              <?php endif; ?>
             </div>
           </div>
           <div class="mt-6 flex items-center gap-x-6">
