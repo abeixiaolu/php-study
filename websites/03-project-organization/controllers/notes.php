045398528bd3a149b6ff3fbbb6f4928fef5adc32
xiaoluabei <xiaoluabei@gmail.com>
2025-01-21 14:21:01 +0800

    03-project-organization

---
+++ websites/03-project-organization/controllers/notes.php
@@ -0,0 +1,7 @@
+<?php
+$heading = "My Notes";
+$config = require "config.php";
+$db = new Database($config['database']);
+
+$notes = $db->query("select * from notes where user_id = 1")->fetchAll();
+require "views/notes.view.php";
