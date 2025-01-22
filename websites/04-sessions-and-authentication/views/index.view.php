f514682e6343d3e8eb2f72a03a7e14bb405a8318
xiaoluabei <xiaoluabei@gmail.com>
2025-01-22 15:36:51 +0800

    hash password

--- websites/04-sessions-and-authentication/views/index.view.php
+++ websites/04-sessions-and-authentication/views/index.view.php
@@ -4,7 +4,10 @@
 <?php require "partials/banner.php"; ?>
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    Home Page
+    <h1 class="text-2xl font-bold">
+      Welcome to my website
+    </h1>
+    <p class="bg-cyan-600 p-2  rounded-full mt-4 text-white">Hello <?= $_SESSION['user']['email'] ?? 'Guest' ?></p>
   </div>
 </main>
 
