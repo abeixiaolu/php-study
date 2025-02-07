ed9a26f3b8eab856a57e0c09696ad5f989507ed6
xiaoluabei <xiaoluabei@gmail.com>
2025-02-07 16:16:20 +0800

    sql query n+1 problem

--- example/resources/views/jobs.blade.php
+++ example/resources/views/jobs.blade.php
@@ -5,11 +5,18 @@
 
   <ul class="space-y-4">
     @foreach ($jobs as $job)
-      <li>
-        <a href="/jobs/{{ $job['id'] }}" class="text-blue-600 hover:underline">
-          <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+        <a href="/jobs/{{ $job['id'] }}" class="block p-4 border border-gray-200 rounded-md">
+          <div class="text-sm text-rose-500 mb-2 font-bold">
+            {{$job->employer->name}}
+          </div>
+          <div><strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.</div>
+          @if ($job->tags->count() > 0)
+            <div
+          class="text-sm text-white bg-blue-700 px-2 py-1/4 rounded-md inline-flex items-center justify-center">
+              {{$job->tags->pluck('name')->join(', ')}}
+            </div>
+          @endif
         </a>
-      </li>
     @endforeach
   </ul>
 </x-layout>
