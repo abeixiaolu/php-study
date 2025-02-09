814c12514934ae12ab4fed865b9c32955c159c05
xiaoluabei <xiaoluabei@gmail.com>
2025-02-09 11:06:36 +0800

    use controller

---
+++ example/app/Http/Controllers/JobController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        return view('jobs.index', [
+            'jobs' => Job::with('employer')->latest()->simplePaginate((3))
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', [
+            'job' => $job
+        ]);
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', [
+            'job' => $job
+        ]);
+    }
+
+    public function destroy(Job $job)
+    {
+        $job->delete();
+        return redirect('/jobs');
+    }
+
+    public function update(Job $job)
+    {
+        request()->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'salary' => ['required'],
+            'description' => [],
+        ]);
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'description' => request('description'),
+        ]);
+        return redirect('/jobs/' . $job->id);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'salary' => ['required'],
+        ]);
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'description' => request('description'),
+            'employer_id' => 1
+        ]);
+        return redirect('/jobs');
+    }
+}
--- example/routes/web.php
+++ example/routes/web.php
@@ -1,69 +1,27 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    return view('jobs.index', [
-        'jobs' => Job::with('employer')->latest()->simplePaginate((3))
-    ]);
-});
+// Route::get('/jobs', [JobController::class, 'index']);
+// Route::get('/jobs/create', [JobController::class, 'create']);
+// Route::get('/jobs/{job}', [JobController::class, 'show']);
+// Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+// Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// Route::patch('/jobs/{job}', [JobController::class, 'update']);
+// Route::post('/jobs', [JobController::class, 'store']);
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::delete('/jobs/{job}', 'destroy');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::post('/jobs', 'store');
+// });
 
-Route::get('/jobs/{id}', function ($id) {
-    return view('jobs.show', [
-        'job' => Job::find($id)
-    ]);
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-    return view('jobs.edit', [
-        'job' => Job::find($id)
-    ]);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-    Job::find($id)->delete();
-    return redirect('/jobs');
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    request()->validate([
-        'title' => ['required', 'min:3', 'max:255'],
-        'salary' => ['required'],
-        'description' => [],
-    ]);
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'description' => request('description'),
-    ]);
-    return redirect('/jobs/' . $id);
-});
-
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3', 'max:255'],
-        'salary' => ['required'],
-    ]);
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'description' => request('description'),
-        'employer_id' => 1
-    ]);
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
